00cb8072c57be296edc89acb2d9711eadb3a3155
sckim <seungchan1219@khu.ac.kr>
2019-10-18 17:33:54 +0900

    10단 추가

--- gugudan.py
+++ gugudan.py
@@ -1,7 +1,7 @@
 gugudan=[]
-for i in range(1,10):
+for i in range(1,11): #10단 추가 
     temp=[]
-    for j in range(1,10):
+    for j in range(1,11):
         temp.append(str((i*j)).center(4," "))
     gugudan.append(temp)
 for elem in gugudan:
